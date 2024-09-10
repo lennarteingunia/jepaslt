bcff70787900c1b79f48f0789832b42584f666fa
Lennart Eing <lennart.eing@uni-a.de>
2024-09-10 11:06:38 +0200

    Updated ravdess configs and job.2

--- jobs/ravdess/ravdess.sh
+++ jobs/ravdess/ravdess.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=vjepa-ravdess
 #SBATCH --time=24-0
-#SBATCH --output=/mnt/slurm/lennart/jepaslt/jepaslt/.slurmlogs/%x-%j.log
+#SBATCH --output=/mnt/slurm/lennart/jepaslt/logs/%x-%j.log
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=40G
 #SBATCH --nodes=1
@@ -12,18 +12,18 @@
 echo "Activating conda."
 source /mnt/data/miniconda3/bin/activate
 
-# Updating conda itself
+# # Updating conda itself
 
-conda update -n base -c defaults conda
+# conda update -n base -c defaults conda
 
-# Upgrading to the libmamba solver
+# # Upgrading to the libmamba solver
 
-conda install -n base conda-libmamba-solver
-conda config --set solver libmamba
+# conda install -n base conda-libmamba-solver
+# conda config --set solver libmamba
 
 # Creating conda environment if it does not already exists
 
-env_yml_path=/mnt/slurm/lennart/jepaslt/jepaslt/environment.yml
+env_yml_path=/mnt/slurm/lennart/jepaslt/environment.yml
 env_name=$(grep 'name:' $env_yml_path | awk '{print $2}')
 env_path=/mnt/data/miniconda3/envs/$env_name
 
@@ -35,20 +35,20 @@ conda activate $env_name
 
 # Actually run the evaluation script
 
-export PYTHONPATH=$PYTHONPATH:/mnt/slurm/lennart/jepaslt/jepaslt/
-python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/jepaslt/configs/evals/vith16_384_ravdess_split_0.yaml --devices cuda:0 cuda:1
+export PYTHONPATH=$PYTHONPATH:/mnt/slurm/lennart/jepaslt
+python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/configs/evals/vith16_384_ravdess_split_0.yaml --devices cuda:0
 
-python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/jepaslt/configs/evals/vith16_384_ravdess_split_1.yaml --devices cuda:0 cuda:1
+python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/configs/evals/vith16_384_ravdess_split_1.yaml --devices cuda:0
 
-python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/jepaslt/configs/evals/vith16_384_ravdess_split_2.yaml --devices cuda:0 cuda:1
+python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/configs/evals/vith16_384_ravdess_split_2.yaml --devices cuda:0
 
-python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/jepaslt/configs/evals/vith16_384_ravdess_split_3.yaml --devices cuda:0 cuda:1
+python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/configs/evals/vith16_384_ravdess_split_3.yaml --devices cuda:0
 
-python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/jepaslt/configs/evals/vith16_384_ravdess_split_4.yaml --devices cuda:0 cuda:1
+python -m evals.main --fname=/mnt/slurm/lennart/jepaslt/configs/evals/vith16_384_ravdess_split_4.yaml --devices cuda:0
 
 # I also need to "unexport" the path, because otherwise I will clutter this env variable.
-export PYTHONPATH=${PYTHONPATH%:/mnt/slurm/lennart/jepaslt/jepaslt/}
+export PYTHONPATH=${PYTHONPATH%:/mnt/slurm/lennart/jepaslt/}
 conda deactivate
 conda env remove -n $env_name
 
-conda config --set solver classic
+# conda config --set solver classic
